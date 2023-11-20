dfe150dc6422b0833b902a8ebccd468759a19fe1
Rezoneter <150760616+Rezoneter@users.noreply.github.com>
2023-11-20 15:10:49 +0900

    add app2 & create app3

---
+++ app2.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def main():
+    st.title('Web Dashboard')
+    
+    print('Web Dashboard')
+    
+    st.text('Develop Webdashboard')
+
+    st.header('This area is header area')
+
+    st.subheader('This area is subheader area')
+
+    st.write('Hello there')
+
+    st.success('When success some action, show text')
+
+    st.warning('warning when have some problem ')
+
+    st.info('show information text')
+
+    st.error('show this text when some error awkard')
+
+
+
+
+if __name__ == '__main__':
+    main()
+# streamlit 에서의 정해진 구문
---
+++ app3.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+# 판다스의 데이터프레임을 웹화면으로 보여주는 방법
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
